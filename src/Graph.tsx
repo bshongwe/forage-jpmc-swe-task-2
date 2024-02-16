2c559b796d724acd9c78202381fde327cc5375fd
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-16 20:03:42 +0200

    Task 2 - JP Morgan & Chase Perspective table configs - line 58 syntax error

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -55,6 +55,7 @@ class Graph extends Component<IProps, {}> {
             elem.setAttribute("columns", '["top_ask_price"]');
             elem.setAttribute("aggregates",
                 '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+	    );
     }
   }
 
